1cf557d5a06a88db37dd70bf4f5f4d46aaf05494
Shahryar Khosravi <s_khosravi@rasana.ir>
2024-08-05 11:30:31 +0330

    WIP: Added signin

--- backend/src/feature/auth/services/auth.service.ts
+++ backend/src/feature/auth/services/auth.service.ts
@@ -1,10 +1,36 @@
+import { env } from "process";
 import { userAuthRepository } from "../../../dependencies";
 import { newUserAuth, validatePassword } from "../../../utils/utils";
+import { IUserAuthRepository } from "../respositories/user-auth.repo";
 import { SigninDTO } from "../routes/dto/signin.dto";
 import { signupDTO } from "../routes/dto/signup.dto";
+import jwt from 'jsonwebtoken';
 
 export class AuthService {
-  signin(signinDTO: SigninDTO) {}
+
+  constructor(private readonly authRepo: IUserAuthRepository) {}
+
+  async signin(signinDTO: SigninDTO): Promise<string> {
+    let user;
+    if(signinDTO.username.match("^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$")) {
+      user = this.authRepo.getByEmail(signinDTO.username);
+    }
+    user = await this.authRepo.getByUsername(signinDTO.username);
+
+    if(!user) {
+      throw Error("User not found");
+    }
+
+    if(user.password !== signinDTO.password) {
+      throw Error("Username or password is wrong!");
+    }
+
+    const jwtPayload = {
+      subjectId: 1,
+      username: user.id,
+    }
+    return jwt.sign(jwtPayload, "SECRET");
+  }
 
   async signup(siginupDTO: signupDTO) {
     if (!validatePassword(siginupDTO.password, siginupDTO.passwordconfrim)) {
