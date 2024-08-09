f613a08033d295216cafcdc4c13b1e623eb3df48
amir.f <amirfarahani10@gmail.com>
2024-08-09 12:44:04 +0330

    feat:

--- frontend/src/app/Login/components/RegisterComponent.tsx
+++ frontend/src/app/Login/components/RegisterComponent.tsx
@@ -5,9 +5,19 @@ import GmailIcon from "../../../assets/images/Icons/gmail.jpg";
 import Vector from "../../../assets/images/Icons/Vector.jpg";
 import MirzaInput from "../../../Shared/Components/MirzaInput";
 import MirzaButton from "../../../Shared/Components/MirzaButton";
+import { useForm } from "react-hook-form";
 export default function RegisterComponent() {
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data: any) => {
+    console.log(data);
+  };
+
   return (
-    <div className="mr-0 w-full content-center py-6">
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="mr-0 w-full content-center py-6"
+    >
       <p
         dir="rtl"
         className="w-400 leading-24px text-right text-sm text-amber-950"
@@ -15,12 +25,34 @@ export default function RegisterComponent() {
         برای ثبت‌نام کافیه نام کاربری، ایمیل و یک رمز عبور وارد کنید:
       </p>
       <div className="flex flex-col justify-center gap-y-6 py-8">
-        <MirzaInput placeholder="نام کاربری" inputIcon={Vector} />
-        <MirzaInput placeholder="ایمیل" inputIcon={GmailIcon} />
-        <MirzaInput placeholder="رمز عبور" inputIcon={keyIcon} />
-        <MirzaInput placeholder="تکرار رمز عبور" inputIcon={keyIcon} />
+        <MirzaInput
+          name="username"
+          register={register}
+          placeholder="نام کاربری"
+          inputIcon={Vector}
+        />
+        <MirzaInput
+          name="email"
+          register={register}
+          placeholder="ایمیل"
+          inputIcon={GmailIcon}
+        />
+        <MirzaInput
+          type="password"
+          name="password"
+          register={register}
+          placeholder="رمز عبور"
+          inputIcon={keyIcon}
+        />
+        <MirzaInput
+          type="password"
+          name="confirmPassword"
+          register={register}
+          placeholder="تکرار رمز عبور"
+          inputIcon={keyIcon}
+        />
       </div>
-      <MirzaButton title="ثبت نام " onClick={() => console.log("click me")} />
-    </div>
+      <MirzaButton title="ثبت نام " type="submit" />
+    </form>
   );
 }
