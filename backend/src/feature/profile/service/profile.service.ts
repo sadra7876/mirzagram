81d299a129d987c9c89f068d96bbb7bdfbb66e4f
sadra7876 <rahmani787601@gmail.com>
2024-08-11 13:05:55 +0330

    add responsedto and swagger

--- backend/src/feature/profile/service/profile.service.ts
+++ backend/src/feature/profile/service/profile.service.ts
@@ -6,9 +6,11 @@ import {
   verifyPassword,
 } from "feature/auth/utils/password.utils";
 import { ProfileId } from "types/profile.type";
-import { profileDTO } from "../dto/profile.dto";
+import { profileRequestDTO, ProfileResponseDTO } from "../dto/profile.dto";
 import { HttpError } from "utils/http-error";
 import { strings } from "resources/strings";
+import omit from "lodash/omit";
+import { Profile } from "../repository/profile.entity";
 dotenv.config();
 
 interface dependencies {
@@ -19,10 +21,24 @@ export class ProfileService {
   constructor(private readonly deps: dependencies) {}
 
   async getUserProfile(id: ProfileId) {
-    return await this.deps.profileRepo.getById(id);
+    const user = await this.deps.profileRepo.getById(id);
+    if (!user) {
+      throw new HttpError(404, strings.USER_NOT_FOUND);
+    }
+    const result: ProfileResponseDTO = {
+      username: user.username,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      email: user.email,
+      isPrivate: user.isActive,
+      bio: user.bio,
+      profilePicture: user.profilePicture,
+      createdAt: user.createdAt,
+    };
+    return result;
   }
 
-  async updateUserProfile(profileDTO: profileDTO, id: ProfileId) {
+  async updateUserProfile(profileDTO: profileRequestDTO, id: ProfileId) {
     const user = await this.deps.profileRepo.getById(id);
     if (!passwordMatch(profileDTO.password, profileDTO.confirmPassword)) {
       throw new HttpError(400, strings.PASSWORDS_DO_NOT_MATCH_ERROR);
