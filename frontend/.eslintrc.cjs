35f8d45a3382db15f363ecf238cd4cb2a53eb33a
amir.f <amirfarahani10@gmail.com>
2024-09-02 23:30:21 +0330

    fix: :bug: fix lint error

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -10,6 +10,8 @@ module.exports = {
   parser: "@typescript-eslint/parser",
   plugins: ["react-refresh"],
   rules: {
+    "no-unused-vars": "off",
+    "no-unused-vars": ["error", { varsIgnorePattern: "^_" }],
     "@typescript-eslint/no-unsafe-optional-chaining": "off",
     "@typescript-eslint/no-explicit-any": "off",
     "react-refresh/only-export-components": [
