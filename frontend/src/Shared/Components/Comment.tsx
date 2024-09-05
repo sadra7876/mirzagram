9d9b425abf260479112e0bb8cd641e8c31ff60f7
ssh.azizi98 <ssh.azizi98@gmail.com>
2024-09-05 18:50:34 +0000

    Bugfix/style

--- frontend/src/Shared/Components/Comment.tsx
+++ frontend/src/Shared/Components/Comment.tsx
@@ -10,44 +10,47 @@ export default function CommentComponent(props: { comments: MirzaComment }) {
       {comments.data.map((item, index) => {
         return (
           <>
-            <div key={index} className="flex w-full flex-row gap-x-20">
-              <div className="flex w-[299px] flex-row gap-2">
-                <p className="text-xs font-bold">
-                  {item.author.displayName || "موجود نیست"}
-                </p>
-                <p className="text-[10px] font-normal text-gray-500">
-                  ۵ هفته پیش
-                </p>
+            <div key={index} className="gap-y-4 py-4">
+              <div className="flex w-full flex-row gap-x-20">
+                <div className="flex w-[299px] flex-row gap-x-2 font-bold">
+                  <p className="text-xs font-bold">
+                    {item.author.displayName || "موجود نیست"}
+                  </p>
+                  <p className="text-[10px] font-normal text-gray-500">
+                    ۵ هفته پیش
+                  </p>
+                </div>
+                <div className="flex flex-row justify-end gap-4">
+                  <LikeComponent initialCount={item.likeCount} />
+                  <p className="text-xs font-bold text-mirza-red">پاسخ</p>
+                  <FaReply className="font-bold text-mirza-red" />
+                </div>
               </div>
-              <div className="flex flex-row justify-end gap-4">
-                <LikeComponent initialCount={item.likeCount} />
-                <p className="text-xs font-bold text-mirza-red">پاسخ</p>
-                <FaReply className="font-bold text-mirza-red" />
-              </div>
+              <div className="text-xs">{item.text}</div>
             </div>
-            <div className="gap-y-4 text-xs">{item.text}</div>
             {item.replies.length > 0 ? (
               item.replies.map((replie, index) => (
                 <>
-                  <div
-                    key={index}
-                    className="flex w-full flex-row gap-x-20 pt-4"
-                  >
-                    <div className="flex w-[299px] flex-row gap-2 pr-12">
-                      <p className="text-xs font-bold">
-                        {replie.author.displayName || "موجود نیست"}
-                      </p>
-                      <p className="text-[10px] font-normal text-gray-500">
-                        ۵ هفته پیش
-                      </p>
+                  <div key={index} className="gap-y-4 py-4">
+                    <div className="flex w-full flex-row flex-wrap gap-x-20">
+                      <div className="flex w-[299px] flex-row gap-2 pr-12">
+                        <p className="text-xs font-bold">
+                          {replie.author.displayName || "موجود نیست"}
+                        </p>
+                        <p className="text-[10px] font-normal text-gray-500">
+                          ۵ هفته پیش
+                        </p>
+                      </div>
+                      <div className="flex flex-row justify-end gap-4">
+                        <LikeComponent initialCount={replie.likeCount} />
+                        <p className="text-xs font-bold text-mirza-red">پاسخ</p>
+                        <FaReply className="font-bold text-mirza-red" />
+                      </div>
                     </div>
-                    <div className="flex flex-row justify-end gap-4">
-                      <LikeComponent initialCount={replie.likeCount} />
-                      <p className="text-xs font-bold text-mirza-red">پاسخ</p>
-                      <FaReply className="font-bold text-mirza-red" />
+                    <div className="w-full pb-4 pr-12 text-xs">
+                      {replie.text}
                     </div>
                   </div>
-                  <div className="gap-y-4 pr-12 text-xs">{replie.text}</div>
                 </>
               ))
             ) : (
