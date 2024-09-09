c83934467c100350cce26326f294cb1549cb4445
ssh.azizi98 <ssh.azizi98@gmail.com>
2024-09-09 04:47:06 +0000

    Bugfix/dashboard design

--- frontend/src/Shared/Components/InputComment.tsx
+++ frontend/src/Shared/Components/InputComment.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { GrSend } from "react-icons/gr";
+import { useUserProfile } from "../../context/UserProfileContext";
 export interface sendComment {
   postId: "string";
   text: "string";
@@ -7,22 +8,32 @@ export interface sendComment {
   isSuccess: boolean;
 }
 
-export default function InputComment(props: { postId: string }) {
+export default function InputComment(props: {
+  postId: string;
+  parentCommentId?: string;
+}) {
   // const [loading, setLoading] = useState(false);
   // const [comments, setComments] = useState<MirzaComment | undefined>(undefined);
   const [commentText, setCommentText] = useState("");
-
+  const { userProfile } = useUserProfile();
   const sendComment = async (postId: string) => {
     const token = localStorage.getItem("token");
-    const dataToSend = { postId, text: commentText };
-    const responsePostComment = await fetch("http://mirza-penguin.dev1403.rahnemacollege.ir:81/api/comment", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`,
+    const dataToSend = {
+      postId,
+      text: commentText,
+      parentCommentId: props.parentCommentId?.toString(),
+    };
+    const responsePostComment = await fetch(
+      "http://mirza-penguin.dev1403.rahnemacollege.ir:81/api/comment",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify(dataToSend),
       },
-      body: JSON.stringify(dataToSend),
-    });
+    );
 
     const resultPostComment = await responsePostComment.json();
     if (resultPostComment.isSuccess) {
@@ -32,6 +43,7 @@ export default function InputComment(props: { postId: string }) {
 
   return (
     <form className="flex flex-row items-center gap-x-1">
+      {props.parentCommentId && <div>@{userProfile?.username}</div>}
       <input
         className="h-9 w-[423px] rounded-2xl border-[1px] border-mirza-gray-comment px-4 py-1"
         placeholder="نظر خود را بنویسید...  "
