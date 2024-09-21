5acc6788e127d6c8ce6ff4213eaa8295b1ccd0a6
amir.f <amirfarahani10@gmail.com>
2024-09-21 22:16:27 +0330

    fix: fix explore style card and page, but not complete data of owner user

--- frontend/src/app/explore/explore.tsx
+++ frontend/src/app/explore/explore.tsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from "react";
-import { FiSearch } from "react-icons/fi";
-import TagComponent from "../../Shared/Components/tagComponent";
 import { getExplore } from "./api/getExplore";
 import { PostDetails } from "../../Shared/model/postDetails.interface";
 import { useNavigate } from "react-router-dom";
-
+import { LuHeart } from "react-icons/lu";
+import { LuMessageCircle } from "react-icons/lu";
+import { CiBookmark } from "react-icons/ci";
 export default function ExplorePage() {
   const [explorePosts, setExplorePosts] = useState<PostDetails[]>();
   const navigate = useNavigate();
@@ -20,127 +20,47 @@ export default function ExplorePage() {
 
   return (
     <div className="flex w-full flex-col gap-y-3 px-3">
-      {/* <div className="flex h-12 w-80 flex-row items-center rounded-full border bg-white px-2">
-        <input
-          placeholder="جستجو در افراد، تگ‌ها، واژه‌ها و..."
-          className="h-full w-full rounded-full"
-        />
-        <FiSearch />
-      </div>
-      <div className="flex w-full flex-wrap gap-2">
-        <TagComponent title="طبیعت" />
-      </div> */}
+      <p>اکسپلور</p>
       <div className="h-screen overflow-auto">
-        <div className="grid grid-cols-2 gap-4 md:grid-cols-3">
-          <div className="grid gap-4">
-            {explorePosts?.map((item, index) => {
-              return (
-                <div
-                  key={index}
-                  className="cursor-pointer"
-                  onClick={() => navigate(`/singlePost?postId=${item.id}`)}
-                >
-                  <img
-                    className="h-auto max-w-full rounded-lg"
-                    src={item.contents[0].url}
-                    alt=""
-                  />
+        <div className="flex flex-wrap gap-4">
+          {explorePosts?.map((item, index) => {
+            return (
+              <div
+                key={index}
+                className="flex h-[437px] w-[304px] cursor-pointer flex-col rounded-lg border bg-white"
+                onClick={() => navigate(`/singlePost?postId=${item.id}`)}
+              >
+                <img
+                  className="h-4/5 max-w-full rounded-t-lg"
+                  src={item.contents[0].url}
+                  alt=""
+                />
+                <div className="flex w-full flex-col justify-center rounded-lg bg-white p-3">
+                  <div className="mb-2 flex gap-3">
+                    <div className="flex items-center justify-center">
+                      <LuMessageCircle />
+                      <p className="text-xs">{item.commentCount}</p>
+                    </div>
+                    <div className="flex items-center justify-center">
+                      <LuHeart />
+                      <p className="text-xs">{item.likeCount}</p>
+                    </div>
+                    <div className="flex items-center justify-center">
+                      <CiBookmark />
+                      <p className="text-xs">{item.bookmarkCount}</p>
+                    </div>
+                  </div>
+                  <div className="flex gap-x-3">
+                    <div className="size-9 rounded-full bg-black"></div>
+                    <div className="flex flex-col gap-y-2">
+                      <p className="text-xs font-extrabold">امیر فراهانی</p>
+                      <p className="text-xs">۱۷۰ هزار دنبال کننده</p>
+                    </div>
+                  </div>
                 </div>
-              );
-            })}
-          </div>
-          {/* <div className="grid gap-4">
-            <div>
-              <img
-                className="h-auto max-w-full rounded-lg"
-                src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg"
-                alt=""
-              />
-            </div>
-            <div>
-              <img
-                className="h-auto max-w-full rounded-lg"
-                src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-1.jpg"
-                alt=""
-              />
-            </div>
-            <div>
-              <img
-                className="h-auto max-w-full rounded-lg"
-                src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-2.jpg"
-                alt=""
-              />
-            </div>
-          </div>
-          <div className="grid gap-4">
-            <div>
-              <img
-                className="h-auto max-w-full rounded-lg"
-                src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-3.jpg"
-                alt=""
-              />
-            </div>
-            <div>
-              <img
-                className="h-auto max-w-full rounded-lg"
-                src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-4.jpg"
-                alt=""
-              />
-            </div>
-            <div>
-              <img
-                className="h-auto max-w-full rounded-lg"
-                src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-5.jpg"
-                alt=""
-              />
-            </div>
-          </div>
-          <div className="grid gap-4">
-            <div>
-              <img
-                className="h-auto max-w-full rounded-lg"
-                src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-6.jpg"
-                alt=""
-              />
-            </div>
-            <div>
-              <img
-                className="h-auto max-w-full rounded-lg"
-                src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-7.jpg"
-                alt=""
-              />
-            </div>
-            <div>
-              <img
-                className="h-auto max-w-full rounded-lg"
-                src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-8.jpg"
-                alt=""
-              />
-            </div>
-          </div>
-          <div className="grid gap-4">
-            <div>
-              <img
-                className="h-auto max-w-full rounded-lg"
-                src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-9.jpg"
-                alt=""
-              />
-            </div>
-            <div>
-              <img
-                className="h-auto max-w-full rounded-lg"
-                src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-10.jpg"
-                alt=""
-              />
-            </div>
-            <div>
-              <img
-                className="h-auto max-w-full rounded-lg"
-                src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-11.jpg"
-                alt=""
-              />
-            </div>
-          </div> */}
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
